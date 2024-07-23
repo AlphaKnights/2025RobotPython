97eff5d44dc949b9220e5223041b1b0141b9fca3
ProEpicGamer1 <proepicgamer1@gmail.com>
2024-07-22 23:15:58 -0700

    begin swerve

--- robot.py
+++ robot.py
@@ -1,62 +1,84 @@
-#!/usr/bin/env python3
-#
+
 # Copyright (c) FIRST and other WPILib contributors.
 # Open Source Software; you can modify and/or share it under the terms of
 # the WPILib BSD license file in the root directory of this project.
 #
 
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring 
+
 import wpilib
 import wpilib.drive
 
+import wpimath
+import wpimath.filter
+import wpimath.controller
+
+from src import drivetrain
+
 
 class MyRobot(wpilib.TimedRobot):
-    def robotInit(self):
-        """
-        This function is called upon program startup and
-        should be used for any initialization code.
-        """
-        self.leftDrive = wpilib.PWMSparkMax(0)
-        self.rightDrive = wpilib.PWMSparkMax(1)
-        self.robotDrive = wpilib.drive.DifferentialDrive(
-            self.leftDrive, self.rightDrive
-        )
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.controller: wpilib.XboxController
+        self.swerve: drivetrain.Drivetrain
+
+        self.xspeed_limiter: wpimath.filter.SlewRateLimiter
+        self.yspeed_limiter: wpimath.filter.SlewRateLimiter
+        self.rot_limiter: wpimath.filter.SlewRateLimiter
+
+    def robotInit(self) -> None:
+        """Robot initialization function"""
         self.controller = wpilib.XboxController(0)
-        self.timer = wpilib.Timer()
+        self.swerve = drivetrain.Drivetrain()
 
-        # We need to invert one side of the drivetrain so that positive voltages
-        # result in both sides moving forward. Depending on how your robot's
-        # gearbox is constructed, you might have to invert the left side instead.
-        self.rightDrive.setInverted(True)
+        # Slew rate limiters to make joystick inputs more gentle; 1/3 sec from
+        # 0 to 1.
+        self.xspeed_limiter = wpimath.filter.SlewRateLimiter(3)
+        self.yspeed_limiter = wpimath.filter.SlewRateLimiter(3)
+        self.rot_limiter = wpimath.filter.SlewRateLimiter(3)
 
-    def autonomousInit(self):
-        """This function is run once each time the robot enters autonomous mode."""
-        self.timer.restart()
+    def autonomousPeriodic(self) -> None:
+        self.drive_with_joystick(False)
+        self.swerve.update_odometry()
 
-    def autonomousPeriodic(self):
-        """This function is called periodically during autonomous."""
+    def teleopPeriodic(self) -> None:
+        self.drive_with_joystick(True)
 
-        # Drive for two seconds
-        if self.timer.get() < 2.0:
-            # Drive forwards half speed, make sure to turn input squaring off
-            self.robotDrive.arcadeDrive(0.5, 0, squareInputs=False)
-        else:
-            self.robotDrive.stopMotor()  # Stop robot
-
-    def teleopInit(self):
-        """This function is called once each time the robot enters teleoperated mode."""
-
-    def teleopPeriodic(self):
-        """This function is called periodically during teleoperated mode."""
-        self.robotDrive.arcadeDrive(
-            -self.controller.getLeftY(), -self.controller.getRightX()
+    def drive_with_joystick(self, field_relative: bool) -> None:
+        # Get the x speed. We are inverting this because Xbox controllers return
+        # negative values when we push forward.
+        x_speed = (
+            -self.xspeed_limiter.calculate(
+                wpimath.applyDeadband(self.controller.getLeftY(), 0.02)
+            )
+            * drivetrain.KMAXSPEED
         )
 
-    def testInit(self):
-        """This function is called once each time the robot enters test mode."""
+        # Get the y speed or sideways/strafe speed. We are inverting this because
+        # we want a positive value when we pull to the left. Xbox controllers
+        # return positive values when you pull to the right by default.
+        y_speed = (
+            -self.yspeed_limiter.calculate(
+                wpimath.applyDeadband(self.controller.getLeftX(), 0.02)
+            )
+            * drivetrain.KMAXSPEED
+        )
 
-    def testPeriodic(self):
-        """This function is called periodically during test mode."""
+        # Get the rate of angular rotation. We are inverting this because we want a
+        # positive value when we pull to the left (remember, CCW is positive in
+        # mathematics). Xbox controllers return positive values when you pull to
+        # the right by default.
+        rot = (
+            -self.rot_limiter.calculate(
+                wpimath.applyDeadband(self.controller.getRightX(), 0.02)
+            )
+            * drivetrain.KMAXSPEED
+        )
 
-
-if __name__ == "__main__":
-    wpilib.run(MyRobot)
+        self.swerve.drive(
+            x_speed,
+            y_speed,
+            rot,
+            field_relative,
+            self.getPeriod())

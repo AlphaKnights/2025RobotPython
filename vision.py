aa36b23d89c5dfe5ab0180116e30ac26ea123967
Snake995544 <jackson.lynch100@gmail.com>
2025-01-22 16:15:11 -0800

    Create vision.py

---
+++ vision.py
@@ -0,0 +1,7 @@
+from cscore import CameraServer
+
+def main():
+    CameraServer.enableLogging
+    camera1 = CameraServer.startAutomaticCapture(0)
+    camera2 = CameraServer.startAutomaticCapture(1)
+    CameraServer.waitForever()

e3adf012f5b30f07499625e24a8216b3730d531e
JSohn-1 <proepicgamer1@gmail.com>
2025-02-06 12:42:02 -0800

    field space

--- interfaces/limelight_results.py
+++ interfaces/limelight_results.py
@@ -5,7 +5,7 @@ class LimelightResults:
 
     def __init__(self, data: dict) -> None:
         self.tag_id = data["Fiducial"][0]["fID"]
-        self.tx = data["Fiducial"][0]['t6t_cs'][0]
-        self.ty = data["Fiducial"][0]['t6t_cs'][2]
-        self.ta = data["Fiducial"][0]['t6t_cs'][4]
+        self.tx = data["Fiducial"][0]['t6t_fs'][0]
+        self.ty = data["Fiducial"][0]['t6t_fs'][2]
+        self.ta = data["Fiducial"][0]['t6t_fs'][4]
         

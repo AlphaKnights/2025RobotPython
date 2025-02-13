f464cf8ef50727b37ef4e14419b2a3c747baea21
IcedTAddict <temourislife100@gmail.com>
2025-02-13 12:01:08 -0800

    ultrasonic partial

--- robot.py
+++ robot.py
@@ -10,6 +10,8 @@ import commands2
 import wpilib
 from wpilib.shuffleboard import Shuffleboard
 from wpilib import SmartDashboard
+import time
+#from wpilib.filter import MedianFilter
 from robotcontainer import RobotContainer
 
 
@@ -21,7 +23,8 @@ class MyRobot(commands2.TimedCommandRobot):
         self.autonomousCommand: Optional[commands2.Command] = None
                 # Creates a ping-response Ultrasonic object on DIO 1 and 2.
         self.rangeFinder = wpilib.Ultrasonic(1, 2)
-        # self.rangeFinder.setAutomaticMode(True)
+        self.rangeFinder.setEnabled(True)
+        self.rangeFinder.setAutomaticMode(True)
 
         # Add the ultrasonic to the "Sensors" tab of the dashboard
         # Data will update automatically
@@ -29,10 +32,13 @@ class MyRobot(commands2.TimedCommandRobot):
 
     def teleopPeriodic(self) -> None:
         # We can read the distance in millimeters
+        #self.rangeFinder.ping()
+        time.sleep(0.1)
         distanceMillimeters = self.rangeFinder.getRangeMM()
         # ... or in inches
-        distanceInches = self.rangeFinder.getRangeInches()
 
+        distanceInches = self.rangeFinder.getRangeInches()
+        distance = self.rangeFinder.getRange()
         print(distanceInches)
 
         # We can also publish the data itself periodically
@@ -59,7 +65,7 @@ class MyRobot(commands2.TimedCommandRobot):
         pass
 
     def testPeriodic(self) -> None:
-        if self.rangeFinder.isRangeValid():
+        #if self.rangeFinder.isRangeValid():
             # Data is valid, publish it
             SmartDashboard.putNumber("Distance[mm]", self.rangeFinder.getRangeMM())
             SmartDashboard.putNumber("Distance[in]", self.rangeFinder.getRangeInches())

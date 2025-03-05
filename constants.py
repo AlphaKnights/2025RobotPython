059d553910ec74dc61949d11c06b05b514802740
IcedTAddict <116768378+IcedTAddict@users.noreply.github.com>
2025-03-04 16:53:39 -0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -54,15 +54,15 @@ class DriveConstants:
 
 
     # SPARK MAX CAN IDs
-    kFrontLeftDrivingCanId = 32
-    kRearLeftDrivingCanId = 1
-    kFrontRightDrivingCanId = 3
-    kRearRightDrivingCanId = 21
+    kFrontLeftDrivingCanId = 6
+    kRearLeftDrivingCanId = 21
+    kFrontRightDrivingCanId = 11
+    kRearRightDrivingCanId = 3
 
-    kFrontLeftTurningCanId = 13
-    kRearLeftTurningCanId = 11
-    kFrontRightTurningCanId = 12
-    kRearRightTurningCanId = 14
+    kFrontLeftTurningCanId = 22
+    kRearLeftTurningCanId = 14
+    kFrontRightTurningCanId = 13
+    kRearRightTurningCanId = 12
 
     kGyroReversed = True
 

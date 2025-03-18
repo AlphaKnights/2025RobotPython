e4fea72bcfe6d2db1a7d86201e1b3d8c4327ab68
IcedTAddict <temourislife100@gmail.com>
2025-03-18 12:53:43 -0700

    elastic

--- variables.py
+++ variables.py
@@ -2,8 +2,3 @@ class varibles:
     is_Aligned = False
     is_Tag_Detected = False
 
-    @classmethod
-    def set_Values(cls, aligned, tag):
-        cls.is_Aligned = aligned
-        cls.is_Tag_Detected = tag
-    
